eb0d647c072866d6a91732e57d782cf4679f39b2
c0nnex <me@c0nnex.de>
2018-03-07 01:08:53 +0100

    Add client script

---
+++ GameMode/Client/clientEvents.ts
@@ -0,0 +1,9 @@
+﻿/// <reference path="../GT-MP/typescripts/index.d.ts" />
+
+API.onServerEventTrigger.connect(function (eventName, args) {
+    var player = API.getLocalPlayer();
+    if (eventName === "GTMPVOICE") {
+        API.voiceEnable(args[0], args[1], args[2], args[3], args[4], args[5]);
+        return;
+    }
+});
